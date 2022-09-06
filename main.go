f271509bee9ae74dfd01dc0b56b8b0a758a72bdc
Jason Tsang <jasontsang.dev@gmail.com>
2022-09-06 17:19:22 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/valyala/fasthttp"
+)
+
+func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
+	method := string(ctx.Method())
+	if method != "OPTIONS" {
+		url := string(ctx.RequestURI())[1:]
+		ctx.Request.SetRequestURI(url)
+
+		hc := &fasthttp.Client{}
+		err := hc.Do(&ctx.Request, &ctx.Response)
+		if err != nil {
+			fmt.Fprintf(ctx, err.Error())
+			return
+		}
+	}
+
+	ctx.Response.Header.Set("Access-Control-Allow-Origin", "*")
+	ctx.Response.Header.Set("Access-Control-Allow-Methods", "*")
+	ctx.Response.Header.Set("Access-Control-Allow-Headers", "*")
+	ctx.Response.Header.Set("Access-Control-Allow-Credentials", "true")
+}
+
+func main() {
+	var port = flag.String("p", "8080", "port")
+
+	flag.Parse()
+
+	fasthttp.ListenAndServe(fmt.Sprintf(":%s", *port), fastHTTPHandler)
+}
